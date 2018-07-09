8baaafc79b6a765ce16fa4a91b1dd1b843b07f66
Kasun Piyumal <kpiyumal90@gmail.com>
2018-07-09 14:39:21 +0530

    splitarator added

--- core/src/main/java/com/blink/core/database/DBCollection.java
+++ core/src/main/java/com/blink/core/database/DBCollection.java
@@ -1,5 +1,14 @@
 package com.blink.core.database;
 
+import java.util.Spliterator;
+import java.util.Spliterators;
+
 public interface DBCollection<T> extends Iterable<T> {
     T first();
+
+    @Override
+    default Spliterator<T> spliterator() {
+        return Spliterators.spliteratorUnknownSize(iterator(), 0);
+    }
+
 }
