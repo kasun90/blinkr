40ea2dc4ad1d22f66fbf062ee34bd1f949e4ac11
Kasun Piyumal <kpiyumal90@gmail.com>
2018-08-30 17:51:19 +0530

    script added functionality for console

--- scripts/blinkrBuild.sh
+++ scripts/blinkrBuild.sh
@@ -27,7 +27,7 @@ git push origin $TAG_NAME
 
 
 cd ..
-cp $SOURCE_DIR/$1/target/blinkr-$2.jar $RELEASE_DIR
+cp $SOURCE_DIR/$1/target/*.jar $RELEASE_DIR
 cp -r $SOURCE_DIR/$1/target/blinkr-$2.lib $RELEASE_DIR
 cp -r $SOURCE_DIR/static/ $RELEASE_DIR
 cp $SOURCE_DIR/blink.conf $RELEASE_DIR
