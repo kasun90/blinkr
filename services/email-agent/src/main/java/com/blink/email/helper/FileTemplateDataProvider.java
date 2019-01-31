6968768afefad79f522769fc391f750781c6be4e
Kasun Piyumal <kpiyumal90@gmail.com>
2019-01-31 16:27:01 +0530

    changed to png

--- services/email-agent/src/main/java/com/blink/email/helper/FileTemplateDataProvider.java
+++ services/email-agent/src/main/java/com/blink/email/helper/FileTemplateDataProvider.java
@@ -17,11 +17,11 @@ public class FileTemplateDataProvider implements EmailTemplateDataProvider {
     public FileTemplateDataProvider(Context context) throws Exception {
         FileService fileService = context.getFileService();
         defaultData.put("blink_logo",
-                fileService.getURL(fileService.newFileURI().appendResource("media/email/logo.svg").build()).toString());
+                fileService.getURL(fileService.newFileURI().appendResource("media/email/logo.png").build()).toString());
         defaultData.put("instagram_icon",
-                fileService.getURL(fileService.newFileURI().appendResource("media/email/icons/instagram.svg").build()).toString());
+                fileService.getURL(fileService.newFileURI().appendResource("media/email/icons/instagram.png").build()).toString());
         defaultData.put("facebook_icon",
-                fileService.getURL(fileService.newFileURI().appendResource("media/email/icons/facebook.svg").build()).toString());
+                fileService.getURL(fileService.newFileURI().appendResource("media/email/icons/facebook.png").build()).toString());
         URIBuilder builder = new URIBuilder();
         Configuration configuration = context.getConfiguration();
         Boolean secured = configuration.getValue("secured", Boolean.class);
